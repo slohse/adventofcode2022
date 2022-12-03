e63b2810278d0e64b5ef76f1cda5d2d3575913b0
Stephan Lohse <dev-github@ploek.org>
2022-12-03 14:54:08 +0100

    day 3 part 2

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -1,7 +1,10 @@
+#![feature(iter_next_chunk)]
+
 use std::fs::File;
 use std::io::prelude::*;
 use clap::Parser;
 use std::path::PathBuf;
+use std::collections::HashSet;
 
 /// Advent of Code Day 03
 #[derive(Parser, Debug)]
@@ -36,9 +39,31 @@ fn item_priority(item: char) -> i64 {
     }
 }
 
+fn common_item(items: &[&str]) -> char {
+    let one: HashSet<char> = items[0].chars().collect();
+    let two: HashSet<char> = items[1].chars().collect();
+    let three: HashSet<char> = items[2].chars().collect();
+
+    let one_two: HashSet<char> = one.intersection(&two).cloned().collect();
+    let all: Vec<&char> = one_two.intersection(&three).collect();
+
+    **all.get(0).unwrap()
+}
+
 fn part1(input: &str) -> i64 {
     input.lines().fold(0, |acc, x| acc + item_priority(find_wrong_item(x).unwrap()))
 }
+
+fn part2(input: &str) -> i64 {
+    let mut iter = input.lines();
+
+    let mut sum = 0;
+    while let Ok(i) = iter.next_chunk::<3>() {
+        sum += item_priority(common_item(&i));
+    }
+    sum
+}
+
 
 fn main() {
     let args = Args::parse();
@@ -48,4 +73,5 @@ fn main() {
     f.read_to_string(&mut input).expect("something went wrong reading the file");
 
     println!("part 1: {}", part1(&input));
+    println!("part 2: {}", part2(&input));
 }
