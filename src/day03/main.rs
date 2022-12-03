901c6711ac39f404627967b7cd78e1ea30d1f670
Stephan Lohse <dev-github@ploek.org>
2022-12-03 10:12:30 +0100

    day 3 part 1

---
+++ src/day03/main.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::prelude::*;
+use clap::Parser;
+use std::path::PathBuf;
+
+/// Advent of Code Day 03
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the input
+    input: PathBuf,
+
+}
+
+
+fn find_wrong_item(rucksack: &str) -> Option<char> {
+    let volume = rucksack.len();
+    let left = &rucksack[0..volume/2];
+    let right = &rucksack[volume/2..volume];
+
+    for item in left.chars() {
+        if right.contains(item) {
+            return Some(item);
+        }
+    }
+    None
+}
+
+fn item_priority(item: char) -> i64 {
+    if item.is_ascii_lowercase() {
+        (item as i64) - 96
+    } else if item.is_ascii_uppercase() {
+        (item as i64) - 38
+    } else {
+        0
+    }
+}
+
+fn part1(input: &str) -> i64 {
+    input.lines().fold(0, |acc, x| acc + item_priority(find_wrong_item(x).unwrap()))
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut f = File::open(args.input).expect("file not found");
+    let mut input = String::new();
+    f.read_to_string(&mut input).expect("something went wrong reading the file");
+
+    println!("part 1: {}", part1(&input));
+}
