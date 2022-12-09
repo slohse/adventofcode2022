48b795da767e42354916a8f92db95505cf0bca8b
Stephan Lohse <dev-github@ploek.org>
2022-12-09 21:42:27 +0100

    day 9, part 2

--- src/day09/main.rs
+++ src/day09/main.rs
@@ -65,7 +65,7 @@ fn calculate_head_move(dir: &str, head: (i64, i64)) -> (i64, i64) {
     }
 }
 
-fn run_input(input: &str) -> usize {
+fn part1(input: &str) -> usize {
     let mut head: (i64, i64) = (0, 0);
     let mut tail = head.clone();
     lazy_static! {
@@ -91,6 +91,37 @@ fn run_input(input: &str) -> usize {
     tailspots.len()
 }
 
+fn part2(input: &str) -> usize {
+    let mut head: (i64, i64) = (0, 0);
+    let mut tail: Vec<(i64, i64)> = Vec::new();
+    for _ in 0..9 {
+        tail.push((0, 0))
+    }
+    lazy_static! {
+        static ref INSTR: Regex = Regex::new(r"^([UDLR]) (\d+)$").unwrap();
+    }
+    let mut tailspots: HashSet<(i64, i64)> = HashSet::new();
+
+    for line in input.lines() {
+        let cap = INSTR.captures(line).unwrap();
+        let dir = cap.get(1).unwrap().as_str();
+        let num = i64_or_bust(cap.get(2).unwrap().as_str());
+
+        for _ in 0..num {
+            head = calculate_head_move(dir, head);
+            let mut cur_knot = head;
+            for knot in tail.iter_mut() {
+                *knot = calculate_tail_move(cur_knot, *knot);
+                cur_knot = *knot;
+            }
+            //println!("head: ({}, {}) -> ({}, {})", head.0, head.1, new_head.0, new_head.1);
+            //println!("tail: ({}, {}) -> ({}, {})", tail.0, tail.1, new_tail.0, new_tail.1);
+            tailspots.insert(cur_knot);
+        }
+    }
+    tailspots.len()
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -98,5 +129,6 @@ fn main() {
     let mut input = String::new();
     f.read_to_string(&mut input).expect("something went wrong reading the file");
 
-    println!("part 1: {}", run_input(&input));
+    println!("part 1: {}", part1(&input));
+    println!("part 2: {}", part2(&input));
 }
